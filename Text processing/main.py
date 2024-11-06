c17868c52029ced4b149335c765283e9bd3452e4
alireza200411 <alirezamoradi292048@gmail.com>
2024-11-06 14:14:26 +0330

    first commit

---
+++ Text processing/main.py
@@ -0,0 +1,33 @@
+﻿def counting_letter(word:str) -> int:
+    index = 0
+    for i in word:
+        if i == ' ':
+            continue
+        else:
+            index += 1
+    else:
+        return f'index: {index}'
+
+
+def counting_word(sentence:str) -> int:
+    letter_list = sentence.split()
+    for item in letter_list:
+        if item.isalpha() == False:
+            letter_list.remove(item)
+        else:
+            continue
+    return f'words: {len(letter_list)}'
+
+
+def counting_character(sentence:str) -> int:
+        letter_list = sentence.split()
+        for item in letter_list:
+            if item.isalpha():
+                continue
+            else:
+                letter_list.remove(item)
+        return f'characters: {len(letter_list)}'
+
+
+
+
